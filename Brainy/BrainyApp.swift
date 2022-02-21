332b58ec0dca685cf7605e54ed4e0fe7b4028fac
George Mapaya <georit01@icloud.com>
2022-02-20 19:11:57 -0500

    Initial Commit

---
+++ Brainy/BrainyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainyApp.swift
+//  Brainy
+//
+//  Created by George Mapaya on 2022-02-20.
+//
+
+import SwiftUI
+
+@main
+struct BrainyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
